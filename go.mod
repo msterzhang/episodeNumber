3bb63166db7ccaac0e8b568c192faa2015aec44e
msterzhang <1144620122@qq.com>
2024-01-03 13:17:04 +0800

    update to v0.0.1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/msterzhang/episodeNumber
+
+go 1.21.2
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func ClearYare(data string) string {
+	regexp, err := regexp.Compile(`(\d+年)`)
+	if err != nil {
+		return data
+	}
+	yareKey := regexp.FindString(data)
+	data = strings.ReplaceAll(data, yareKey, "")
+	return data
+}
+
+func ClearNumber(numbers [][]string) string {
+	dataNumber := ""
+	for _, item := range numbers {
+		numberText := item[1]
+		if len(numberText) > 0 {
+			dataNumber = numberText
+		}
+	}
+	return dataNumber
+}
+
+func EpisodeNumber(text string) (string, error) {
+	regexp, err := regexp.Compile(`(\d{2,3})\D`)
+	if err != nil {
+		return "", err
+	}
+	text = strings.ReplaceAll(text, "1080", "")
+	text = strings.ReplaceAll(text, "720", "")
+	data := ClearYare(text)
+	datas := regexp.FindAllStringSubmatch(data, -1)
+	number := ClearNumber(datas)
+	return number, nil
+}
+
+func SeasonEpisode(name, season, text string) (string, error) {
+	eNumber, err := EpisodeNumber(text)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%sS%sE%s", name, season, eNumber), nil
+}
